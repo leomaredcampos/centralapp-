822e33f96074b5127d545bf37a6d4a93366725a8
leomaredcampos <leomaredcampos.11@gmail.com>
2026-05-30 16:05:10 +0800

    Add email debug logging

--- backend/utils/email.go
+++ backend/utils/email.go
@@ -2,6 +2,7 @@ package utils
 
 import (
 	"fmt"
+	"log"
 	"net/smtp"
 	"os"
 )
@@ -18,8 +19,14 @@ func SendOTPEmail(to, otp string) error {
 		smtpPort = "587"
 	}
 
+	log.Printf("Sending email - User: %s, Host: %s, Port: %s, Password length: %d", user, smtpHost, smtpPort, len(password))
+
 	msg := fmt.Sprintf("From: %s\r\nTo: %s\r\nSubject: Your OTP Code\r\n\r\nYour OTP code is: %s", user, to, otp)
 
 	auth := smtp.PlainAuth("", user, password, smtpHost)
-	return smtp.SendMail(smtpHost+":"+smtpPort, auth, user, []string{to}, []byte(msg))
+	err := smtp.SendMail(smtpHost+":"+smtpPort, auth, user, []string{to}, []byte(msg))
+	if err != nil {
+		log.Printf("Email send error: %v", err)
+	}
+	return err
 }
