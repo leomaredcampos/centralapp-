7c3a557580ed8e615a910fdc5723e535516004f3
leomaredcampos <leomaredcampos.11@gmail.com>
2026-06-06 04:13:22 +0800

    Increase mobile breakpoint to 1024px

--- frontend/src/app/dashboard/hooks/useOrientation/index.ts
+++ frontend/src/app/dashboard/hooks/useOrientation/index.ts
@@ -7,7 +7,7 @@ export function useOrientation() {
 
   useEffect(() => {
     const check = () => {
-      const mobile = window.innerWidth < 768;
+      const mobile = window.innerWidth < 1024;
       setIsPortrait(mobile);
 
       if (mobile) {
